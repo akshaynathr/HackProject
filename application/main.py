bf88dd7c48c40c5b4b5359adb0da46cc234bed76
Akshaynath R <akshaynathr@gmail.com>
2018-07-26 00:19:35 +0530

    added support for db with pymongo

--- application/main.py
+++ application/main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
-
+from flask_pymongo import PyMongo
+import json
 
 app = Flask(__name__)
+app.config["MONGO_URI"]="mongodb://localhost:27017/davinciDb"
 
-
+mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/dashboard')
@@ -30,7 +32,13 @@ def form():
 
 
 
-@app.route('/recrods')
+@app.route('/records')
 def records():
     notification = True
     return render_template('dashboard/records_counter.html')
+
+
+@app.route('/db')
+def dbtest():
+    test = mongo.db.Account.find()
+    return str(test[0])
